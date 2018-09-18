2ea65f341e696e5dbacc2583144ee1d722f05ec3
Amanda <a@zalee.me>
2018-09-18 14:26:58 -0700

    Add app react component

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -2,6 +2,8 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import configureStore from './store/store';
 
+import App from './components/app';
+
 import {receiveTodo, receiveTodos} from './actions/todo_actions';
 
 document.addEventListener('DOMContentLoaded', () => {
@@ -11,5 +13,5 @@ document.addEventListener('DOMContentLoaded', () => {
   window.receiveTodos = receiveTodos;
 
   const root = document.getElementById('root');
-  ReactDOM.render(<h1>Todos App</h1>, root);
+  ReactDOM.render(<App />, root);
 });
