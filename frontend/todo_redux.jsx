3580f57d1d19f1a357ab96eeab38dd595e48a61a
Amanda <a@zalee.me>
2018-09-18 15:27:14 -0700

    Add todos selector

--- frontend/todo_redux.jsx
+++ frontend/todo_redux.jsx
@@ -5,13 +5,15 @@ import configureStore from './store/store';
 import Root from './components/root';
 
 import {receiveTodo, receiveTodos} from './actions/todo_actions';
+import {allTodos} from './reducers/selectors';
 
 document.addEventListener('DOMContentLoaded', () => {
   const store = configureStore();
   window.store = store;
   window.receiveTodo = receiveTodo;
   window.receiveTodos = receiveTodos;
+  window.allTodos = allTodos;
 
   const root = document.getElementById('root');
-  ReactDOM.render(<Root />, root);
+  ReactDOM.render(<Root store={store} />, root);
 });
