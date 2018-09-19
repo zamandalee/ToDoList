00b634da2655e4152db8489f647e2e5840267659
Amanda <a@zalee.me>
2018-09-19 11:42:42 -0700

    Add complete/undo todo logic

--- frontend/components/todo_list/todo_list_item.jsx
+++ frontend/components/todo_list/todo_list_item.jsx
@@ -3,21 +3,39 @@ import React from 'react';
 class TodoListItem extends React.Component {
   constructor(props) {
     super(props);
+
+    let text = this.props.todo.done ? 'undo' : 'complete';
+    this.state = Object.assign( {}, this.props.todo, {'doneText': text} );
+
     this.handleDelete = this.handleDelete.bind(this);
+    this.handleToggle = this.handleToggle.bind(this);
+    // this.doneText = this.doneText.bind(this);
   }
 
   handleDelete() {
     this.props.removeTodo(this.props.todo);
   }
 
+  handleToggle() {
+    this.state['done'] ?
+      this.setState( {done: false, doneText: 'complete'} ) :
+      this.setState( {done: true, doneText: 'undo'} );
+    console.log(this.state.done);
+  }
+
   render() {
+    const { todo } = this.props;
+
     return (
       <li className="todo-list-item">
         <div className="todo-content">
-          <div className="item-title">{ this.props.todo.title }</div>
-          <div className="item-body">{ this.props.todo.body }</div>
+          <div className="item-title">{ todo.title }</div>
+          <div className="item-body">{ todo.body }</div>
         </div>
-        <button className="delete-bttn" onClick={ this.handleDelete }>🗑️</button>
+        <div>
+          <button onClick={ this.handleToggle }>{this.state.doneText}</button>
+          <button className="delete-bttn" onClick={ this.handleDelete }>🗑️</button>
+        </div>
       </li>);
   }
 }
